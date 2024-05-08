fff7a3f8d76e725294e9791870a0a6f0049a98a5
AfShan <119497593+afshansif@users.noreply.github.com>
2024-05-08 07:45:46 +0500

    Created OTP Confirmation Activity

--- app/src/main/java/com/example/chatsy/LoginScreenActivity.java
+++ app/src/main/java/com/example/chatsy/LoginScreenActivity.java
@@ -2,7 +2,9 @@ package com.example.chatsy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class LoginScreenActivity extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class LoginScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(LoginScreenActivity.this, OtpConfirmationActivity.class));
+
+            }
+        },2000);
     }
 }
